3507c352335f1b073f62448f4329151c5c47db29
Alba Gaitan <albagaitan.fsk@gmail.com>
2025-12-29 17:37:55 -0300

    fix  workflow cicd

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -6,10 +6,6 @@ function fn() {
   var baseUrl = java.lang.System.getenv('BASE_URL');
   var apiKey  = java.lang.System.getenv('API_KEY');
 
-  karate.log('BASE_URL-c:', baseUrl);
-  karate.log('API_KEY exists:', !!apiKey);
-
-
   if (!baseUrl || !apiKey) {
     throw 'Missing environment variables BASE_URL or API_KEY';
   }
